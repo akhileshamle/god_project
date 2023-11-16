3690ebb5d50389d46d5e846a9b46c7c937fdf87c
santoryu24 <amleakhilesh@gmail.com>
2023-11-17 00:48:49 +0530

    Setup network layer - part 1

---
+++ god_project/god_project/Network/APIManagerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  APIManagerProtocol.swift
+//  god_project
+//
+//  Created by Akhilesh Amle on 17/11/23.
+//
+
+import Foundation
---
+++ god_project/god_project/Network/RequestManager.swift
@@ -0,0 +1,8 @@
+//
+//  RequestManager.swift
+//  god_project
+//
+//  Created by Akhilesh Amle on 17/11/23.
+//
+
+import Foundation
