d9d3a7ba39d6814d5a51dcca9cacd29b5bdf6c15
santoryu24 <amleakhilesh@gmail.com>
2023-11-17 00:50:32 +0530

    fix commit

--- god_project/god_project/Network/APIManagerProtocol.swift
+++ god_project/god_project/Network/APIManagerProtocol.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+protocol APIManagerProtocol {
+    func perform(
+        _ request: RequestProtocol,
+        authToken: String
+    ) async throws -> Data
+}
+
+class APIManager: APIManagerProtocol {
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    func perform(
+        _ request: RequestProtocol,
+        authToken: String = ""
+    ) async throws -> Data {
+        let (data, response) = try await urlSession.data(
+            for: request.createURLRequest(authToken: authToken)
+        )
+        
+        guard let urlResponse = response as? HTTPURLResponse, urlResponse.statusCode == 200 else {
+            throw NetworkError.invalidServerResponse
+        }
+        return data
+    }
+}
--- god_project/god_project/Network/RequestManager.swift
+++ god_project/god_project/Network/RequestManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol RequestManagerProtocol {
+    func perform<T: Decodable>(_ request: RequestProtocol) async throws -> T
+}
+
+class RequestManager: RequestManagerProtocol {
+    let apiManager: APIManagerProtocol
+    let parser: DataParserProtocol
+    
+    init(
+        apiManager: APIManagerProtocol = APIManager(),
+        parser: DataParserProtocol = DataParser()
+    ) {
+        self.apiManager = apiManager
+        self.parser = parser
+    }
+    
+    func perform<T>(_ request: RequestProtocol) async throws -> T where T : Decodable {
+        let data = try await apiManager.perform(request, authToken: "")
+        return data as! T
+    }
+}
+
+protocol AccessTokenManagerProtocol {}
+class AccessTokenManager: AccessTokenManagerProtocol {}
+
+protocol DataParserProtocol {}
+class DataParser: DataParserProtocol {}
